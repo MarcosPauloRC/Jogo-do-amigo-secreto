83d03700e9f62e807b0941351b88a1ce61c3f820
Marcos Paulo Ribeiro Coelho <ribeirocoelhomarcospaulo@gmail.com]>
2025-02-15 15:19:01 -0300

    jogo do amigo secreto

---
+++ app.js
@@ -0,0 +1,14 @@
+// The main objective of this challenge is to strengthen your programming logic skills. Here you should develop the logic to solve the problem.
+let amigos = [];
+
+function adicionarAmigo() {
+    const nome = Input.Value.Trim ();
+    if (nome === "") {
+        alert("Digite um nome para adicionar um amigo");
+        return false;
+    }   else (nome !== "")
+        alert("ensira um nome");
+}
+
+  
+
